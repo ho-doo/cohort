b69d4c25ea9ee5e93bd75325915431465128ebba
ho-doo <star10317013@gmail.com>
2016-03-18 12:15:05 +0900

    add_revenue_per_actor.sql : Revenue Per Actor From sakila db

--- Revenue Per Film.sql
+++ Revenue Per Film.sql
@@ -1,11 +1,28 @@
+DROP TEMPORARY TABLE IF EXISTS revenue_per_film;
+CREATE TEMPORARY TABLE revenue_per_film
 SELECT
 f. film_id,
 title,
-f. rental_rate * COUNT(*) totla_revenue 
+f. rental_rate * COUNT(*) total_revenue 
 FROM
  rental r, film f, inventory i
 WHERE
  r. inventory_id = i. inventory_id
  AND i.film_id = f.film_id
  GROUP BY 1
+ ;
+ 
+ SELECT
+ actor. actor_id,
+ CONCAT( actor. first_name," ", actor. last_name ),
+ SUM(revenue_per_film. total_revenue)
+ FROM
+ revenue_per_film, film_actor, actor
+ WHERE
+ revenue_per_film. film_id = film_actor. film_id
+ AND film_actor. actor_id = actor. actor_id 
+ GROUP BY 
+ actor. actor_id
+ ORDER BY total_revenue DESC
+ LIMIT 10
  ;
