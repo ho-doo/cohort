ea85abe91c719ea2e31c77303b9257a8380476fd
ho-doo <star10317013@gmail.com>
2016-03-21 11:29:59 +0900

    cohort size

--- cohort analytics.sql
+++ cohort analytics.sql
@@ -7,3 +7,12 @@ FROM
   rental r
 GROUP BY 1
 ;
+
+DROP TEMPORARY TABLES IF EXISTS cohort_size
+CREATE TEMPORARY TABLES cohort_size
+SELECT
+	LEFT(first_time, 7) "month",
+	COUNT(*) num
+FROM first_rental
+GROUP BY 1
+;
