37ca6604e3c7cc50501f27e0a3d74fdeed73a79c
ho-doo <star10317013@gmail.com>
2016-03-23 11:32:15 +0900

    add join on

--- cohort analytics.sql
+++ cohort analytics.sql
@@ -1,34 +1,32 @@
-DROP TEMPORARY TABLES IF EXISTS first_rental;
+DROP TEMPORARY TABLE IF EXISTS first_rental;
 CREATE TEMPORARY TABLE first_rental
 SELECT
-  r. customer_id,
-  MIN(r.rental_date) first_time
-FROM
-  rental r
+	r.customer_id,
+	MIN(r.rental_date) first_time
+FROM rental r
 GROUP BY 1
 ;
 
 DROP TEMPORARY TABLE IF EXISTS cohort_size;
-CREATE TEMPsORARY TABLE cohort_size
+CREATE TEMPORARY TABLE cohort_size
 SELECT
-    LEFT(first_time, 7) Month,
-    COUNT(*) num
+	LEFT(fr. first_time, 7) month,
+	COUNT(*) num
 FROM
-    first_rental
+	first_rental fr
 GROUP BY 1
 ;
 
-SELECT 
-    r.*,
-    LEFT(fr.first_time, 7) cohort,
-    p.amount
+SELECT
+	r.*,
+	cs.month cohort,
+	p. amount
 FROM
-    rental r, 
-    first_rental fr,
-    cohort_size cs,
-    payment p
-WHERE 
-    r.customer_id = fr.customer_id
-    AND cs.month = LEFT(fr.first_time, 7)
-    AND p.rental_id = r.rental_id
-;
+	rental r
+	JOIN first_rental fr
+	ON r.customer_id = fr. customer_id
+	JOIN cohort_size cs
+	ON cs.month = LEFT(fr. first_time, 7)
+	JOIN payment p
+	ON p.rental_id = r.rental_id
+	;
