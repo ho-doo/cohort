85ed7b84150f2cfe52e6d026c3eb76a79c26211d
ho-doo <star10317013@gmail.com>
2016-03-23 12:05:55 +0900

    add cohort

--- cohort analytics.sql
+++ cohort analytics.sql
@@ -30,3 +30,23 @@ FROM
 	JOIN payment p
 	ON p.rental_id = r.rental_id
 	;
+	
+	DROP TEMPORARY TABLE IF EXISTS cohort;
+CREATE TEMPORARY TABLE cohort
+SELECT
+    date_format(fr.first_time, "%Y%m") cohort_formatted,  # cohort month
+    date_format(r.rental_date, "%Y%m") rental_date_formated,  # rental month
+    cs.num cohort_size,
+    SUM(p.amount) month_revenue,
+    SUM(p.amount)/cs.num RPU
+FROM
+    rental r 
+        JOIN first_rental fr
+            ON r.customer_id = fr.customer_id
+        JOIN cohort_size cs
+            ON cs.month = LEFT(fr.first_time, 7)
+        JOIN payment p
+            ON p.rental_id = r.rental_id
+GROUP BY 1, 2
+;
+
